118061bc754a874bed40f1b63e57fc19b819c64e
Marius <inf89478@stud.uni-stuttgart.de>
2019-03-23 11:48:13 +0100

    changed descriptions for functions, calculated nearest edge new and better

--- server/src/Graph.cpp
+++ server/src/Graph.cpp
@@ -10,6 +10,9 @@ void Graph::sortEdges() {
   std::sort(edges.begin(), edges.end(), edgesComparer);
 }
 
+/**
+ * generates the offset for the graph
+ */
 void Graph::genOffset() {
   int i = 0;
   int n = 0;
@@ -29,6 +32,7 @@ void Graph::genOffset() {
 
 // returns nearest node in vector graph.nodes to the input coords (lon, lat)
 Node Graph::getNearestNode(double lon, double lat) {
+  std::cout << "lon: " << lon << ", lat: " << lat << std::endl;
   std::vector<Node>::iterator it = nodes.begin();
   Node result = *it;
   double distance = sqrt(pow((lon - it->lon), 2.0) + pow((lat - it->lat), 2.0));
@@ -45,7 +49,9 @@ Node Graph::getNearestNode(double lon, double lat) {
   return result;
 }
 
-// returns nearest edge in vector graph.edges to the input coords (lon, lat)
+/**
+ *  returns nearest edge in vector graph.edges to the input coords (lon, lat)
+ */
 Edge Graph::getNearestEdge(double lon, double lat) {
   std::vector<Edge>::iterator it = edges.begin();
   Edge result = *it;
@@ -56,11 +62,24 @@ Edge Graph::getNearestEdge(double lon, double lat) {
 
   it++;
   for (; it != edges.end(); it++) {
-    currentDistance = abs((src.lat - src.lat) * lon - (src.lon - src.lon) * lat + src.lon * src.lat - tgt.lat * src.lon) / sqrt(pow(tgt.lat - src.lat, 2) + pow(tgt.lon - src.lon, 2));
+    src = nodes[it->srcNodeId];
+    tgt = nodes[it->tgtNodeId];
+    currentDistance = abs((tgt.lat - src.lat) * lon - (tgt.lon - src.lon) * lat + tgt.lon * src.lat - tgt.lat * src.lon) / sqrt(pow(tgt.lat - src.lat, 2) + pow(tgt.lon - src.lon, 2));
     if (currentDistance < distance) {
+      std::cout << "currentDistance: " << currentDistance << std::endl;
+      std::cout << "distance: " << distance << std::endl;
       distance = currentDistance;
       result = *it;
     }
   }
+  std::cout << "result: " << result.srcNodeId << " " << result.tgtNodeId << std::endl;
   return result;
+}
+
+/**
+ * calculates the distance of an edge
+ * and writes it to its distance member variable
+ */
+void calcEdgeDistance(Edge edge) {
+  GeographicLib::Geodesic::WGS84().Inverse(nodes[edge.srcNodeId].lat, nodes[edge.srcNodeId].lon, nodes[edge.tgtNodeId].lat, nodes[edge.tgtNodeId].lon, edge.distance);
 }
