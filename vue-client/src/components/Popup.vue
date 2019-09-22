00645d1f651422c923b70e8ad951487fdbd0b50e
Marius Altmann <dodeca@gmx.de>
2019-09-22 16:45:51 +0200

    building a popup overlay for map is not used

---
+++ vue-client/src/components/Popup.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div id="popup"></div>
+  </div>
+</template>
+
+<script>
+import Overlay from "ol/Overlay.js";
+
+export default {
+  name: "Popup",
+  data() {
+    return {
+      popup: null
+    };
+  },
+  mounted() {
+    this.popup = new Overlay({
+      element: document.getElementById("popup")
+    });
+  }
+};
+</script>
+
+<style>
+</style>
