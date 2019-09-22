b275e9de3b81761c94d7aa7f2ed24ea03661ab28
Marius Altmann <dodeca@gmx.de>
2019-09-22 16:48:23 +0200

    added vuetify and process.env.NODE_ENV

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -1,10 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store/store";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+window.vueDevMode = process.env.NODE_ENV;
+window.serverUrl = location.protocol + "//" + location.host + location.pathname;
+
+if (window.vueDevMode === "development") {
+  window.serverUrl = location.protocol + "//localhost:8091/";
+}
 
 new Vue({
   store,
+  vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
