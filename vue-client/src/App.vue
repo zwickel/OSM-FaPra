5d5e653059de1420929b46f0b50820cfe8926ee0
Marius Altmann <dodeca@gmx.de>
2019-09-22 16:44:08 +0200

    removed map and added toolbar and set up a layout with v-cards

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,27 +1,33 @@
 <template>
-  <div id="app">
-    <Map/>
-  </div>
+  <v-app>
+    <v-app-bar id="toolbar" app dark>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>OpenStreetMap</span>
+        <v-spacer></v-spacer>
+        <span class="font-weight-light">Meeting point finder</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <Map />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import Map from "./components/Map.vue";
+import Map from "./components/Map";
 
 export default {
-  name: "app",
+  name: "App",
   components: {
     Map
-  }
+  },
+  data: () => ({
+    //
+  })
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  /* margin-top: 60px; */
-}
-</style>
